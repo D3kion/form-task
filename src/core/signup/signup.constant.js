1038fdb36480a37d64b54ac4497d18995c865724
D3kion <dekion.dev@gmail.com>
2021-05-13 21:23:19 +0300

    lesson 1

---
+++ src/core/signup/signup.constant.js
@@ -0,0 +1,2 @@
+export const SIGNUP_ROUTE_PATH = '/signup';
+// export const SIGNUP_ROUTE_PATH_DYNAMIC = () => '/signup';
--- src/main/router/index.js
+++ src/main/router/index.js
@@ -1,5 +1,8 @@
+import { SignupRouter, SIGNUP_ROUTE_PATH } from '../../core/signup';
 import { routesInit } from './router.core';
 
-export const routes = {};
+export const routes = {
+  [SIGNUP_ROUTE_PATH]: SignupRouter,
+};
 
 export const Router = routesInit(routes);
